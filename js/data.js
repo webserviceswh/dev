74e540b4b814970dc7c6e58c2677add026012f07
Wolfgang <wolfgang.heindl.wh@gmail.com>
2017-10-22 12:14:25 +0200

    Final Version 1

--- js/data.js
+++ js/data.js
@@ -1,3 +1,14 @@
 var petrasEvents = {
-	'10-10-2017' : '<a class="red"  href="#">Yoga - Beginner <br> 18:00 - 19:00</a><br><a class="green"  href="#">Yoga - Beginner <br> 18:00 - 19:00</a>',
+	'10-02-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
+	'10-09-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a><a class="blue"  href="#yogaAdvanced">18:00 - 19:00 Yoga - Fortgeschrittene</a>',
+	'10-16-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
+	'10-23-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
+	'10-30-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
+	'11-06-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
+	'11-13-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
+	'11-20-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
+	'11-27-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
+	'12-04-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
+	'12-11-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
+	'12-18-2017' : '<a class="red"  href="#yogaBeginner">18:00 - 19:00 Yoga - Anfänger </a>',
 };
